b379e1261df0347e7c0e6d57d744c8d08b80e962
Pavan Shetty <43493028+pkshetty011@users.noreply.github.com>
2022-08-16 15:11:20 +0530

    Update main.tf

--- examples/terraform-project/code/main.tf
+++ examples/terraform-project/code/main.tf
@@ -22,6 +22,23 @@ resource "aws_instance" "web_app" {
   }
 }
 
+
+resource "aws_instance" "web_app" {
+  ami           = "ami-674cbc1e"
+  instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+
+  root_block_device {
+    volume_size = 50
+  }
+
+  ebs_block_device {
+    device_name = "my_data"
+    volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
+    volume_size = 500
+    iops        = 800
+  }
+}
+
 resource "aws_lambda_function" "hello_world" {
   function_name = "hello_world"
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
